c40cebac69c885594204e24910d8db517c150b06
samuel-adusei <samuel-adusei@hotmail.com>
2017-05-24 17:00:50 +0100

    first

---
+++ intro.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!-- first got to sort php and mysql -->
+<!-- sort the front page with catelogue and imagery-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title> angular </title>
+</head>
+<body>
+
+<div ng-app="">
+<h1> hello </h1>
+
+
+<script type= "text/javascript" src="javascript.js"> </script>
+
+</body>
+</html>	
